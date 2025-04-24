c4f275c01f15680e45b1d9f625f621d7b775f405
duartemiguel30 <122054876+duartemiguel30@users.noreply.github.com>
2025-04-24 13:16:47 +0100

    done

--- rules_auto.pl
+++ rules_auto.pl
@@ -1,29 +1,2 @@
 :- op(800, fx, if).
 :- op(700, xfx, then).
-if(\+ fact(dm_7, sim) , \+ fact(dm_6, sim) , \+ fact(ag_3, sim) , \+ fact(aa_14, sim)) then amarelo:1.0.
-if(\+ fact(dm_7, sim) , \+ fact(dm_6, sim) , \+ fact(ag_3, sim) , fact(aa_14, sim) , \+ fact(aa_3, sim)) then amarelo:1.0.
-if(\+ fact(dm_7, sim) , \+ fact(dm_6, sim) , \+ fact(ag_3, sim) , fact(aa_14, sim) , fact(aa_3, sim)) then laranja:1.0.
-if(\+ fact(dm_7, sim) , \+ fact(dm_6, sim) , fact(ag_3, sim) , \+ fact(emb_3, sim) , \+ fact(tia_4, sim)) then vermelho:1.0.
-if(\+ fact(dm_7, sim) , \+ fact(dm_6, sim) , fact(ag_3, sim) , \+ fact(emb_3, sim) , fact(tia_4, sim)) then verde:1.0.
-if(\+ fact(dm_7, sim) , \+ fact(dm_6, sim) , fact(ag_3, sim) , fact(emb_3, sim)) then amarelo:1.0.
-if(\+ fact(dm_7, sim) , fact(dm_6, sim) , \+ fact(aa_5, sim) , \+ fact(dm_2, sim) , \+ fact(dm_9, sim)) then laranja:1.0.
-if(\+ fact(dm_7, sim) , fact(dm_6, sim) , \+ fact(aa_5, sim) , \+ fact(dm_2, sim) , fact(dm_9, sim)) then verde:1.0.
-if(\+ fact(dm_7, sim) , fact(dm_6, sim) , \+ fact(aa_5, sim) , fact(dm_2, sim) , \+ fact(aa_9, sim)) then laranja:1.0.
-if(\+ fact(dm_7, sim) , fact(dm_6, sim) , \+ fact(aa_5, sim) , fact(dm_2, sim) , fact(aa_9, sim)) then vermelho:1.0.
-if(\+ fact(dm_7, sim) , fact(dm_6, sim) , fact(aa_5, sim) , \+ fact(emb_2, sim) , \+ fact(me_1, sim)) then vermelho:1.0.
-if(\+ fact(dm_7, sim) , fact(dm_6, sim) , fact(aa_5, sim) , \+ fact(emb_2, sim) , fact(me_1, sim)) then amarelo:1.0.
-if(\+ fact(dm_7, sim) , fact(dm_6, sim) , fact(aa_5, sim) , fact(emb_2, sim) , \+ fact(me_6, sim)) then vermelho:1.0.
-if(\+ fact(dm_7, sim) , fact(dm_6, sim) , fact(aa_5, sim) , fact(emb_2, sim) , fact(me_6, sim)) then verde:1.0.
-if(fact(dm_7, sim) , \+ fact(aa_14, sim) , \+ fact(tia_2b, sim) , \+ fact(me_3, sim) , \+ fact(dm_2, sim)) then laranja:1.0.
-if(fact(dm_7, sim) , \+ fact(aa_14, sim) , \+ fact(tia_2b, sim) , \+ fact(me_3, sim) , fact(dm_2, sim)) then verde:1.0.
-if(fact(dm_7, sim) , \+ fact(aa_14, sim) , \+ fact(tia_2b, sim) , fact(me_3, sim) , \+ fact(dm_3, sim)) then verde:1.0.
-if(fact(dm_7, sim) , \+ fact(aa_14, sim) , \+ fact(tia_2b, sim) , fact(me_3, sim) , fact(dm_3, sim)) then verde:1.0.
-if(fact(dm_7, sim) , \+ fact(aa_14, sim) , fact(tia_2b, sim) , \+ fact(dm_4, sim)) then amarelo:1.0.
-if(fact(dm_7, sim) , \+ fact(aa_14, sim) , fact(tia_2b, sim) , fact(dm_4, sim)) then vermelho:1.0.
-if(fact(dm_7, sim) , fact(aa_14, sim) , \+ fact(dm_8, sim) , \+ fact(dm_4, sim) , \+ fact(aa_9, sim)) then vermelho:1.0.
-if(fact(dm_7, sim) , fact(aa_14, sim) , \+ fact(dm_8, sim) , \+ fact(dm_4, sim) , fact(aa_9, sim)) then vermelho:1.0.
-if(fact(dm_7, sim) , fact(aa_14, sim) , \+ fact(dm_8, sim) , fact(dm_4, sim)) then laranja:1.0.
-if(fact(dm_7, sim) , fact(aa_14, sim) , fact(dm_8, sim) , \+ fact(dm_1, sim) , \+ fact(dm_5, sim)) then verde:1.0.
-if(fact(dm_7, sim) , fact(aa_14, sim) , fact(dm_8, sim) , \+ fact(dm_1, sim) , fact(dm_5, sim)) then laranja:1.0.
-if(fact(dm_7, sim) , fact(aa_14, sim) , fact(dm_8, sim) , fact(dm_1, sim) , \+ fact(dm_3, sim)) then amarelo:1.0.
-if(fact(dm_7, sim) , fact(aa_14, sim) , fact(dm_8, sim) , fact(dm_1, sim) , fact(dm_3, sim)) then vermelho:1.0.
